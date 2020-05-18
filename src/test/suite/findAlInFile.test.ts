bc8f0195ce36759fc5e4d03bb37f5260f6574f46
Benbuck Nason <bnason@netflix.com>
2020-05-18 15:51:42 -0700

    Add more detailed find result tests

--- src/test/suite/findAlInFile.test.ts
+++ src/test/suite/findAlInFile.test.ts
@@ -1,5 +1,9 @@
-//import * as assert from "assert";
-import { before, beforeEach } from "mocha";
+// Copyright 2019 Benbuck Nason
+
+"use strict";
+
+import * as assert from "assert";
+import { afterEach, before, beforeEach } from "mocha";
 import * as mock from "ts-mockito";
 import * as path from "path";
 import * as vscode from "vscode";
@@ -11,9 +15,20 @@ import { IOutputSink } from "../../iOutputSink";
 const outputMock: IOutputSink = mock.mock<IOutputSink>();
 const output: IOutputSink = mock.instance(outputMock);
 
+function isEqual(fr: FindResult, text: string, line: number, columnBegin: number, columnEnd: number)
+{
+	return (fr.line == line) && (fr.columnBegin == columnBegin) && (fr.columnEnd == columnEnd) && (fr.text == text);
+}
+
 suite("No document", () => {
 	beforeEach(() => {
 		mock.reset(outputMock);
+	});
+
+	afterEach(() => {
+		mock.verify(outputMock.begin(mock.anything(), mock.anyString(), mock.anyOfClass(Boolean),
+			mock.anyOfClass(Boolean), mock.anyOfClass(Boolean))).never();
+		mock.verify(outputMock.end()).never();
 	});
 
 	test("String case", () => {
@@ -76,6 +91,12 @@ suite("Find", async () => {
 		findStringCase(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", false, true, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(6);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -83,6 +104,9 @@ suite("Find", async () => {
 		findStringCaseWord(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", false, true, true)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(3);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -90,6 +114,16 @@ suite("Find", async () => {
 		findStringNoCase(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", false, false, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(10);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(7)[0], "Test tEst TeSttEST", 3, 5, 9));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(8)[0], "Test tEst TeSttEST", 3, 10, 14));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(9)[0], "Test tEst TeSttEST", 3, 14, 18));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -97,22 +131,36 @@ suite("Find", async () => {
 		findStringNoCaseWord(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", false, false, true)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(5);
-		mock.verify(outputMock.end()).once();
-	});
-
-	test("Regex case", () => {
-		findRegexCase(doc, "test", output);
-		mock.verify(outputMock.begin(doc, "test", true, true, false)).once();
-		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(6);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "Test tEst TeSttEST", 3, 5, 9));
 		mock.verify(outputMock.end()).once();
 	});
 
 	// Trivial regex tests
 
+	test("Regex case", () => {
+		findRegexCase(doc, "test", output);
+		mock.verify(outputMock.begin(doc, "test", true, true, false)).once();
+		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(6);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		mock.verify(outputMock.end()).once();
+	});
+
 	test("Regex case word", () => {
 		findRegexCaseWord(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", true, true, true)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(3);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -120,6 +168,16 @@ suite("Find", async () => {
 		findRegexNoCase(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", true, false, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(10);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(7)[0], "Test tEst TeSttEST", 3, 5, 9));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(8)[0], "Test tEst TeSttEST", 3, 10, 14));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(9)[0], "Test tEst TeSttEST", 3, 14, 18));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -127,6 +185,11 @@ suite("Find", async () => {
 		findRegexNoCaseWord(doc, "test", output);
 		mock.verify(outputMock.begin(doc, "test", true, false, true)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(5);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "Test tEst TeSttEST", 3, 5, 9));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -136,6 +199,14 @@ suite("Find", async () => {
 		findRegexCase(doc, "t.st", output);
 		mock.verify(outputMock.begin(doc, "t.st", true, true, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(8);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "no tast no TAST no tASt", 2, 3, 7));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(7)[0], "Test tEst TeSttEST", 3, 5, 9));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -143,6 +214,19 @@ suite("Find", async () => {
 		findRegexNoCase(doc, "t.st", output);
 		mock.verify(outputMock.begin(doc, "t.st", true, false, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(13);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "no tast no TAST no tASt", 2, 3, 7));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(7)[0], "no tast no TAST no tASt", 2, 11, 15));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(8)[0], "no tast no TAST no tASt", 2, 19, 23));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(9)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(10)[0], "Test tEst TeSttEST", 3, 5, 9));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(11)[0], "Test tEst TeSttEST", 3, 10, 14));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(12)[0], "Test tEst TeSttEST", 3, 14, 18));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -150,6 +234,8 @@ suite("Find", async () => {
 		findRegexCase(doc, "test.*", output);
 		mock.verify(outputMock.begin(doc, "test.*", true, true, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(2);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "testno 456 notest def notest", 1, 0, 28));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -157,6 +243,9 @@ suite("Find", async () => {
 		findRegexNoCase(doc, "test.*", output);
 		mock.verify(outputMock.begin(doc, "test.*", true, false, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(3);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "testno 456 notest def notest", 1, 0, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "Test tEst TeSttEST", 3, 0, 18));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -164,6 +253,13 @@ suite("Find", async () => {
 		findRegexCase(doc, "t[ae]st", output);
 		mock.verify(outputMock.begin(doc, "t[ae]st", true, true, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(7);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "no tast no TAST no tASt", 2, 3, 7));
 		mock.verify(outputMock.end()).once();
 	});
 
@@ -171,6 +267,19 @@ suite("Find", async () => {
 		findRegexNoCase(doc, "t[ae]st", output);
 		mock.verify(outputMock.begin(doc, "t[ae]st", true, false, false)).once();
 		mock.verify(outputMock.item(mock.anyOfClass(FindResult))).times(13);
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(0)[0], "test 123 test abc test", 0, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(1)[0], "test 123 test abc test", 0, 9, 13));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(2)[0], "test 123 test abc test", 0, 18, 22));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(3)[0], "testno 456 notest def notest", 1, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(4)[0], "testno 456 notest def notest", 1, 13, 17));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(5)[0], "testno 456 notest def notest", 1, 24, 28));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(6)[0], "no tast no TAST no tASt", 2, 3, 7));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(7)[0], "no tast no TAST no tASt", 2, 11, 15));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(8)[0], "no tast no TAST no tASt", 2, 19, 23));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(9)[0], "Test tEst TeSttEST", 3, 0, 4));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(10)[0], "Test tEst TeSttEST", 3, 5, 9));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(11)[0], "Test tEst TeSttEST", 3, 10, 14));
+		assert(isEqual(mock.capture(outputMock.item).byCallIndex(12)[0], "Test tEst TeSttEST", 3, 14, 18));
 		mock.verify(outputMock.end()).once();
 	});
 });
